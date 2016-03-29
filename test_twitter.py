bd59623a97e56a701f3ad379884bc4ee24d4c10a
Darrick Chung <chungkyd@gmail.com>
2016-03-29 00:28:14 -0700

    added parameter testing for new status route and destroy route

--- test_twitter.py
+++ test_twitter.py
@@ -1,4 +1,5 @@
-import pytest, oauth2, urlparse, json, os
+import pytest, urlparse, json, os, oauth2, base64
+# from restkit import *
 from user_keys import UserKeys
 from selenium import webdriver
 from selenium.webdriver import Chrome
@@ -15,7 +16,7 @@ def webdriver(request): #fxn name to be inputed to test. 'request' is a special 
 
 #OAuth Authentication for a user to make API calls
 @pytest.fixture(scope='function')
-def auth1(webdriver):
+def auth1():
 	user = UserKeys().user1()
 	consumer = oauth2.Consumer(key=user["CONSUMER_KEY"], secret=user["CONSUMER_SECRET"])
 	access_token = oauth2.Token(key=user["ACCESS_KEY"], secret=user["ACCESS_SECRET"])
@@ -25,7 +26,7 @@ def auth1(webdriver):
 
 # OAuth Authentication for a second user
 @pytest.fixture(scope='function')
-def auth2(webdriver):
+def auth2():
 	user = UserKeys().user2()
 	consumer = oauth2.Consumer(key=user["CONSUMER_KEY"], secret=user["CONSUMER_SECRET"])
 	access_token = oauth2.Token(key=user["ACCESS_KEY"], secret=user["ACCESS_SECRET"])
@@ -35,8 +36,12 @@ def auth2(webdriver):
 # Posts a new tweet. Takes OAuth obj, and message str
 @pytest.fixture(scope='function')
 def create():
-	def create_status(client, message):
+	def create_status(client, message, **kwargs):
 		endpoint = "https://api.twitter.com/1.1/statuses/update.json?status=" + message
+		# concatenate additional params to url
+		for params in kwargs:
+			endpoint += ("&" + str(params) + "=" + kwargs[params])
+		
 		response, data = client.request(endpoint,"POST")
 		return json.loads(data)
 	return create_status
@@ -44,8 +49,10 @@ def create():
 # Destroys a tweet. Takes OAuth obj, and tweet_id
 @pytest.fixture(scope='function')
 def destroy():
-	def destroy_status(client, tweet_id):
-		endpoint = "https://api.twitter.com/1.1/statuses/destroy/"+tweet_id+".json"
+	def destroy_status(client, tweet_id, **kwargs):
+		endpoint = "https://api.twitter.com/1.1/statuses/destroy/"+tweet_id+".json?"
+		for params in kwargs:
+			endpoint += ("&" + str(params) + "=" + kwargs[params])
 		response, data = client.request(endpoint,"POST")
 		return json.loads(data)
 	return destroy_status
@@ -60,18 +67,66 @@ def test_user_timeline(auth1):
 
 @pytest.mark.statuses
 def test_post_new_status(auth1,create,destroy):
-
 	data = create(auth1, "Hello%20World")
 	destroy(auth1,data["id_str"])
 
-	assert not "errors" in data and data["text"] == "Hello World", "Expected Hello World message in 'text' key"
+	assert not "errors" in data  
+	assert data["text"] == "Hello World", "Expected Hello World message in 'text' key"
+
+@pytest.mark.statuses
+def test_post_new_status_char_limit(auth1,create,destroy):
+	tweet = "ilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechickenilovechicken"
+	data = create(auth1, tweet)
+
+	assert "errors" in data 
+	assert data["errors"][0]['message'] == "Status is over 140 characters." 
+
+@pytest.mark.statuses
+def test_post_new_status_long_not_provided(auth1,create,destroy):
+	data = create(auth1, "Hello%20World", lat = "10")
+	assert "errors" in data, "Expected errors"
+	assert data["errors"][0]["message"] == 'Invalid coordinates.', "Expected error msg"
+
+@pytest.mark.statuses
+def test_post_new_status_lat_not_provided(auth1,create,destroy):
+	data = create(auth1, "Hello%20World", long = "10")
+
+	assert "errors" in data, "Expected errors"
+	assert data["errors"][0]["message"] == 'Invalid coordinates.', "Expected error msg"
+
+@pytest.mark.statuses
+def test_post_new_status_lat_out_of_range(auth1,create,destroy):
+	data = create(auth1, "Hello%20World", lat = "200")
+	if not "errors" in data:
+		destroy(auth1,data["id_str"])
+
+	assert "errors" in data, "Expected errors"
+	assert data["errors"][0]["message"] == 'Invalid coordinates.', "Expected error msg"
+
+@pytest.mark.statuses
+def test_post_new_status_lng_out_of_range(auth1,create,destroy):
+	data = create(auth1, "Hello%20World", long = "200")
+	if not "errors" in data:
+		destroy(auth1,data["id_str"])
+
+	assert "errors" in data, "Expected errors"
+	assert data["errors"][0]["message"] == 'Invalid coordinates.', "Expected error msg"
 
 @pytest.mark.statuses
 def test_destroy_status(auth1, create, destroy):
 	new_status = create(auth1, "Hello%20World")
 	data = destroy(auth1,new_status["id_str"])
 
-	assert not "errors" in data and data["id_str"] == new_status["id_str"], "Expected no errors and for 'id_str' value to match"
+	assert not "errors" in data 
+	assert data["id_str"] == new_status["id_str"], "Expected no errors and for 'id_str' value to match"
+
+@pytest.mark.statuses
+def test_destroy_status_params_trim_user(auth1, create, destroy):
+	new_status = create(auth1, "Hello%20World")
+	data = destroy(auth1,new_status["id_str"],trim_user="true")
+
+	assert not "errors" in data
+	assert len(data["user"]) <= 2, "Expected user key to only have 2 values"
 
 @pytest.mark.statuses
 def test_retweet(auth1, auth2, create, destroy):
@@ -99,6 +154,7 @@ def test_undo_retweet(auth1,auth2,create,destroy):
 	destroy(auth1,new_status1["id_str"])
 
 	assert id_str == unretweet_id_str, "Expected retweet's id_str to equal unretweet's id_str"
+
 
 
 
